6536e426993394c0234f2a83ea2fa740ae39cc4b
theriddling@gmail.com <theriddling@gmail.com@7ff223ff-e4eb-e95a-2f50-886f60ef44b0>
2012-09-19 08:05:12 +0000

    prototyping: add commands 'location' and 'acceleration' that can work with an arbitrary number of files to analyze them

--- trunk/prototyping/python/analyze.py
+++ trunk/prototyping/python/analyze.py
@@ -2,46 +2,18 @@ import SBF, sys, os.path
 import pylab as plt
 from numpy import *
 
-if len(sys.argv) < 2:
-	print 'usage: python analyze.py rec_{timecode}'
-	sys.exit(0)
+__usage__ = 'usage: python analyze.py location|acceleration PATH [PATH ...]'
 
-files = []
-for f in sys.argv[1:]:
-	try: files.append(SBF.decode(f))
-	except IOError as e: print(e)
-
-if len(files) == 0: sys.exit(1)
-
-
-tloc,lat,long,alt,acc = range(5)
-tacc,x,y,z = range(4)
-
-if len(files) > 1:
-	plt.hold(1)
-	for f in files:
-		d = SBF.get_all_attributes(f["location"])
-		plt.plot(d[lat],d[long])
-	plt.show()
-	
-	for f in files:
-		d = SBF.get_all_attributes(f["acceleration"])
-		plt.hold(1)
-		plt.plot(d[tacc],d[x])
-		plt.plot(d[tacc],d[y])
-		plt.plot(d[tacc],d[z])
-		plt.show()
-	
-else:
-	loc = files[0]["location"]
+def filter_location(loc, n=3, m=None):
+	if not m: m = (n+1) / 2.
 	t,lat,long,alt,acc = SBF.get_all_attributes(loc)
 	pos = array([lat,long])
-	filtered = []
-	n = 3
-	m = n / 2.
+	
 	deltapos = array([0,0])
 	macc = acc[:n]
 	mpos = (pos[:,:n] * macc).sum(1) / macc.sum()
+	
+	filtered = []
 	filtered = [mpos]
 	for i in range(n+1,pos.shape[1]):
 		macc_new = acc[i-n:i]
@@ -54,8 +26,41 @@ else:
 		deltat = (t[i-1] - t[i-2]) / 1000
 		mpos = mpos + deltapos * deltat * m
 		filtered.append(mpos)
+	
 	filtered = array(filtered)
+	return lat,long,filtered[:,0],filtered[:,1]
+
+if len(sys.argv) < 3:
+	print __usage__
+	sys.exit(0)
+	
+command = sys.argv[1]
+
+if not command in ["acceleration","location"]:
+	print 'command',command,'not understood'
+	print __usage__
+	sys.exit(1)
+
+files = []
+for f in sys.argv[2:]:
+	try: files.append(SBF.decode(f))
+	except IOError as e: print(e)
+
+if len(files) == 0: sys.exit(1)
+
+if command == "acceleration":
+	for f in files:
+		d = SBF.get_all_attributes(f["acceleration"])
+		plt.hold(1)
+		plt.plot(d[0],d[1])
+		plt.plot(d[0],d[2])
+		plt.plot(d[0],d[3])
+		plt.show()
+
+elif command == "location":
 	plt.hold(1)
-	plt.plot(lat,long)
-	plt.plot(filtered[:,0],filtered[:,1])
+	for f in files:
+		lat,long,s_lat,s_long = filter_location(f["location"],n=1)
+		#plt.plot(lat,long)
+		plt.plot(s_lat,s_long)
 	plt.show()
